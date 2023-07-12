0d619bc6d0ca9fcba904273aed7af35be9b40e3e
Cebrian10 <aldaircebrian@hotmail.com>
2023-07-12 14:31:29 -0500

    Creando pagina de articulos y modal de comentarios

--- src/app/articulo/articulo.page.ts
+++ src/app/articulo/articulo.page.ts
@@ -6,10 +6,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./articulo.page.scss'],
 })
 export class ArticuloPage implements OnInit {
+  comentarios: any[] = [];
+  nuevoComentario: string = "";
 
   constructor() { }
 
   ngOnInit() {
   }
+
+  agregarComentario(nuevoComentario: string) {
+    const comentario = {
+      contenido: nuevoComentario,
+      usuario: {
+        nombre: "Nombre del usuario" // Reemplaza "Nombre del usuario" con el nombre real obtenido de algún lugar
+      }
+    };
+    this.comentarios.push(comentario);
+    this.nuevoComentario = "";
+  }
+  
 
 }
