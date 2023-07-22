7c1e28b6176ebdec76059b18567dd230953b68d7
Polar <67519178+Polar1920@users.noreply.github.com>
2023-07-21 22:38:53 -0500

    QR Scanner mejorado
    -implementacion directa
    -sin phonegap

--- src/app/tabs/tabs.page.ts
+++ src/app/tabs/tabs.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { BarcodeScanner } from '@awesome-cordova-plugins/barcode-scanner/ngx';
 
 @Component({
   selector: 'app-tabs',
@@ -8,12 +9,19 @@ import { Component } from '@angular/core';
 export class TabsPage {
   selectedTab: string;
 
-  constructor() {
+  constructor(private barcodeScanner: BarcodeScanner) {
     this.selectedTab = 'home'; // Inicialización predeterminada
   }
 
   tabChanged(event: any) {
     this.selectedTab = event.tab;
+  }
+
+  async scan() {
+    const result = await this.barcodeScanner.scan();
+    if (!result.cancelled) {
+      console.log('El Articulo es: ', result.text);
+    }
   }
 
 }
