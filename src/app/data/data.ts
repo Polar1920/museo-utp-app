87bf7db1e48d54fb6148352aa3bc68aff85a1e21
David Pollard <dmpp1920@gmail.com>
2023-07-23 16:21:36 -0500

    Login, Account and Localdata
    -Funcionalidad del checkbox
    -Recordar usuario
    -Funcionalidades y manejo de los datos del usuario localmente
    -Logout
    -Pantalla Account
    -Cambio de asset
    -Comprobacion de foto de usuario

--- src/app/data/data.ts
+++ src/app/data/data.ts
@@ -7,7 +7,7 @@ import { HttpClient } from '@angular/common/http';
 export class Data {
   apiUrl = 'https://ds6.glaciar.club/api';
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) { }
 
   getUsers() {
     return this.http.get(`${this.apiUrl}/usuarios/all`);
@@ -17,15 +17,40 @@ export class Data {
     return this.http.get(`${this.apiUrl}/usuarios/${id}`);
   }
 
-  loginUser(nombre_usuario: String,password: String){
+  loginUser(nombre_usuario: String, password: String) {
     return this.http.post(`${this.apiUrl}/auth/login`, { nombre_usuario, password });
   }
-  registerUser(nombre_usuario:string,password:string,nombre:string,apellido:string,cedula:string,nivel:number,id_facultad:number,id_carrera:number,foto:string){
-    return this.http.post(`${this.apiUrl}/usuarios`, {nombre_usuario,password,nombre,apellido,cedula,nivel,id_facultad,id_carrera,foto});
+
+  reUser(nombre_usuario: String, password: String) {
+    localStorage.setItem('log_nombre_usuario', nombre_usuario.toString());
+    localStorage.setItem('log_password', password.toString());
+    let user = localStorage.getItem('log_nombre_usuario');
+    let pass = localStorage.getItem('log_password');
+    console.log('Se recordara el usuario: ' + user + ", " + pass);
   }
+
+  noReUser() {
+    localStorage.removeItem('log_nombre_usuario');
+    localStorage.removeItem('log_password');
+    let user = localStorage.getItem('log_nombre_usuario');
+    let pass = localStorage.getItem('log_password');
+    console.log('No se recordara el usuario' + user + ", " + pass);
+  }
+
+  Logout() {
+    localStorage.removeItem('usuario');
+    localStorage.removeItem('token');
+    console.log('Sesión finalizada'+localStorage.getItem('usuario')+localStorage.getItem('token'));
+  }
+
+  registerUser(nombre_usuario: string, password: string, nombre: string, apellido: string, cedula: string, nivel: number, id_facultad: number, id_carrera: number, foto: string) {
+    return this.http.post(`${this.apiUrl}/usuarios`, { nombre_usuario, password, nombre, apellido, cedula, nivel, id_facultad, id_carrera, foto });
+  }
+
   getCarreras() {
     return this.http.get(`${this.apiUrl}/carreras/all`);
   }
+
   getFacultades() {
     return this.http.get(`${this.apiUrl}/facultades/all`);
   }
