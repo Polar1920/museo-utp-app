3d1c727472a295c22072e33297797411ba3482e3
David Pollard <dmpp1920@gmail.com>
2023-07-24 08:45:04 -0500

    act - front - nelson - rivaldo

--- src/app/narration/narration.page.ts
+++ src/app/narration/narration.page.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-narration',
@@ -7,9 +8,62 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NarrationPage implements OnInit {
 
-  constructor() { }
+  conversacion: { emisor: boolean, mensaje: string }[] = [];
+  mensajes: string[] = [
+    'Sí, son conocidas por su apariencia y su sistema operativo intuitivo.',
+    'Una de las cosas destacadas de las Macintosh es su interfaz gráfica fácil de usar.',
+    '¡Eso es genial! Me encanta su diseño.',
+  ];
+  mensajeActual: number = 0;
+  @ViewChild('conversacionesContainer', { static: false })
+  conversacionesContainer!: ElementRef;
+
+  constructor(private navCtrl: NavController) { }
 
   ngOnInit() {
+    // Mensajes inicial
+    this.agregarMensajeInicial(true, 'La Apple Macintosh es reconocida por su diseño elegante y facilidad de uso.');
+    this.agregarMensajeInicial(false, 'Sí, son conocidas por su apariencia y su sistema operativo intuitivo.');
   }
 
+  agregarMensajeInicial(emisor: boolean, mensaje: string) {
+    this.conversacion.push({ emisor, mensaje });
+  }
+
+  agregarMensaje() {
+    if (this.mensajeActual < this.mensajes.length) {
+      const mensaje = this.mensajes[this.mensajeActual];
+      const emisor = this.mensajeActual % 2 === 0; // Alterna entre emisor y receptor en cada mensaje
+      this.conversacion.push({ emisor, mensaje });
+      this.mensajeActual++;
+      // Desplazar el scroll para mostrar el mensaje recién agregado
+      this.scrollDown();
+      // Desplazar automáticamente después de un breve retraso
+      setTimeout(() => this.scrollDownAutomatically(), 100);
+    }
+  }
+
+
+  scrollDown() {
+    const element = document.getElementById('contenedor-conversaciones');
+    if (element) {
+      element.scrollTop = element.scrollHeight;
+    }
+  }
+  scrollDownAutomatically() {
+    const element = this.conversacionesContainer.nativeElement;
+    const lastMessage = element.lastElementChild;
+    if (lastMessage) {
+      lastMessage.scrollIntoView({ behavior: 'smooth', block: 'end' });
+    }
+  }
+
+
+  navegarAtras() {
+    this.navCtrl.back();
+  }
+
+  hayMensajesPorAgregar() {
+    return this.mensajeActual < this.mensajes.length;
+  }
 }
