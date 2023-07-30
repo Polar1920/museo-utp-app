1fd5ff429066c7ad3bb66a2e1ee0de257f5d891a
Nvar21 <neja200211@gmail.com>
2023-07-29 23:29:43 -0500

    Actualización página de de narración

--- src/app/narration/narration.page.ts
+++ src/app/narration/narration.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { NavController } from '@ionic/angular';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-narration',
@@ -15,34 +16,38 @@ export class NarrationPage implements OnInit {
   conversaciones: any[] = [];
 
   conversacion: { emisor: boolean, mensaje: string }[] = [];
-  mensajes: string[] = [
-    'Sí, son conocidas por su apariencia y su sistema operativo intuitivo.',
-    'Una de las cosas destacadas de las Macintosh es su interfaz gráfica fácil de usar.',
-    '¡Eso es genial! Me encanta su diseño.',
-  ];
-  mensajeActual: number = 0; 
+  mensajes: string[] = [];
+
+  mensajeActual: number = 0;
   @ViewChild('conversacionesContainer', { static: false })
   conversacionesContainer!: ElementRef;
 
-  constructor(private navCtrl: NavController) { }
+  constructor(
+    private navCtrl: NavController,
+    private http: HttpClient // Agregar el servicio HttpClient
+  ) { }
 
   ngOnInit() {
-    this.titulo = localStorage.getItem('narTitulo');
-    this.foto = localStorage.getItem('narFoto');
-    this.categoria = localStorage.getItem('narCategoria');
-    this.year = localStorage.getItem('narYear');
+    // Obtener el artículo ID del almacenamiento local (localStorage)
+    const articulo_id = localStorage.getItem('articulo_id');
 
-    this.conversaciones = localStorage.getItem('conversaciones')?.split('|') || [];
+    // Verificar si hay un ID de artículo válido
+    if (articulo_id) {
+      // Obtener datos de la API utilizando el ID de artículo
+      const apiURL = `https://ds6.glaciar.club/api/articulos/${articulo_id}`;
+      this.http.get<any>(apiURL).subscribe(data => {
+        // Asignar los datos de la API a las propiedades correspondientes
+        this.titulo = data.nombre;
+        this.foto = data.fotos[0].url;
+        this.categoria = data.categoria_id; // Dependiendo de cómo se llame en la respuesta de la API
+        this.year = new Date(data.created_at).getFullYear(); // Obtener el año desde la fecha de creación
+        this.mensajes = data.descripcion.split('*')[1].split('|');
 
-    console.log(this.titulo);
-    console.log(this.foto);
-    console.log(this.categoria);
-    console.log(this.year);
-
-    console.log(this.conversaciones);
-    // Mensajes inicial
-    this.agregarMensajeInicial(true, 'La Apple Macintosh es reconocida por su diseño elegante y facilidad de uso.');
-    this.agregarMensajeInicial(false, 'Sí, son conocidas por su apariencia y su sistema operativo intuitivo.');
+      });
+    } else {
+      // Manejar el caso en que no hay un ID de artículo válido
+      console.error('ID de artículo no encontrado en el almacenamiento local.');
+    }
   }
 
   agregarMensajeInicial(emisor: boolean, mensaje: string) {
@@ -62,13 +67,13 @@ export class NarrationPage implements OnInit {
     }
   }
 
-
   scrollDown() {
     const element = document.getElementById('contenedor-conversaciones');
     if (element) {
       element.scrollTop = element.scrollHeight;
     }
   }
+
   scrollDownAutomatically() {
     const element = this.conversacionesContainer.nativeElement;
     const lastMessage = element.lastElementChild;
@@ -77,12 +82,11 @@ export class NarrationPage implements OnInit {
     }
   }
 
-
   navegarAtras() {
     this.navCtrl.back();
   }
 
- hayMensajesPorAgregar() {
-  return this.mensajeActual < this.mensajes.length;
-}
+  hayMensajesPorAgregar() {
+    return this.mensajeActual < this.mensajes.length;
+  }
 }
