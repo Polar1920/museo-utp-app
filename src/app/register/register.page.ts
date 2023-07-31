2de10829124173c32f57e7405c7e318905e3742a
David Pollard <dmpp1920@gmail.com>
2023-07-30 21:52:20 -0500

    qr y fix en registro

--- src/app/register/register.page.ts
+++ src/app/register/register.page.ts
@@ -16,20 +16,6 @@ import { Platform, NavController } from '@ionic/angular';
 })
 export class RegisterPage implements OnInit {
 
-  //para mostrar la segunda parte del form
-  mostrarSegundaParte = false;
-
-  mostrarSegundaParteForm() {
-    this.mostrarSegundaParte = true;
-  }
-
-  regresarPrimeraParte() {
-    this.mostrarSegundaParte = false;
-  }
-  
-
-  //host: string = environment.apiBaseUrl;
-
   name: string = '';
   username: string = '';
   lastName: string = '';
@@ -46,6 +32,27 @@ export class RegisterPage implements OnInit {
   carreras: any = [];
   facultades: any = [];
   errors: string[] = [];
+
+  //para mostrar la segunda parte del form
+  mostrarSegundaParte = false;
+
+  mostrarSegundaParteForm() {
+    if (this.username.length < 4) {
+      alert('El nombre de usuario debe tener al menos 4 caracteres.');
+    } else if (this.name.length < 4) {
+      alert('Llene nombre correctamente.');
+    } else if (this.lastName.length < 4) {
+      alert('Llene apellido correctamente.');
+    } else if (this.cedula.length < 7) {
+      alert('Cédula - Error de formato.');
+    } else {
+      this.mostrarSegundaParte = true;
+    }
+  }
+
+  regresarPrimeraParte() {
+    this.mostrarSegundaParte = false;
+  }
 
 
   constructor(private router: Router, private data: Data, private platform: Platform, private navController: NavController) { }
@@ -95,22 +102,43 @@ export class RegisterPage implements OnInit {
 
   submitRegister() {
     console.log('Datos del registro: Nombre:', this.name, this.lastName, this.selectedFacultad, this.selectedCarrera, this.password, this.passwordRepet);
-    this.data.registerUser(this.username, this.password, this.name, this.lastName, this.cedula, this.level, this.selectedFacultad, this.selectedCarrera, this.foto).subscribe(
-      (response) => {
-        // Manejar la respuesta del servidor en caso de éxito
-        console.log('Registro exitoso:', response);
-        alert('Registro exitoso.');
-        this.goToLogin();
-        // Redirigir al usuario a la página de inicio de sesión
-      },
-      (error) => {
-        // Manejar cualquier error que se produzca
-        console.log('Error en el Login:', error.errors);
-        alert(error.errors);
-      }
-    );
+    if (this.level === 0) {
+      alert('Debe seleccionar un nivel.');
+      return;
+    } else if (this.selectedFacultad === 0) {
+      alert('Debe seleccionar una facultad.');
+      return;
+    } else if (this.selectedCarrera === 0) {
+      alert('Debe seleccionar una carrera.');
+      return;
+    } else if (this.password !== this.passwordRepet) {
+      alert('Las contraseñas no coinciden.');
+      return;
+    } else {
+      this.data.registerUser(this.username, this.password, this.name, this.lastName, this.cedula, this.level, this.selectedFacultad, this.selectedCarrera, this.foto).subscribe(
+        (response) => {
+          // Manejar la respuesta del servidor en caso de éxito
+          console.log('Registro exitoso:', response);
+          alert('Registro exitoso.');
+          this.goToLogin();
+          // Redirigir al usuario a la página de inicio de sesión
+        },
+        (error) => {
+          // Manejar cualquier error que se produzca
+          console.log('Error en el Registro:', error);
+          let errorMessage = 'Se produjo un error al registrar al usuario.';
+          errorMessage += '\n';
+          for (let i = 0; i < error.error.errors.length; i++) {
+            errorMessage += error.error.errors[i].msg + '\n';
+          }
+          setTimeout(() => {
+            alert(errorMessage);
+          }, 0);
+        }
+      );
+    }
   }
-  
+
   goToLogin() {
     this.router.navigate(['/login']);
   }
